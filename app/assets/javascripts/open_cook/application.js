a434a471f110da158432831eb8a533bcdcd64528
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-07-04 17:56:02 +0400

    ~~~

---
+++ app/assets/javascripts/open_cook/application.js
@@ -0,0 +1,23 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery.ui.core
+//= require jquery.ui.sortable
+
+//= require jquery.ui.nestedSortable
+//= require sortable_tree/initializer
+
+//= require expandable_tree/restorable
+//= require expandable_tree/initializer
+
+//= require redactor
+//= require redactor_ru
+//= require redactor/initialize
+
+//= require jquery.MultiFile
+//= require jquery.Jcrop
+
+//= require logger
+//= require the_comments
+//= require the_comments_manage
+
+//= require crop_image
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,9 @@ class WelcomeController < ApplicationController
     per_count = 7
 
     @hubs   = Hub.of_(:posts).with_states(:published)
-    @posts  = Post.active_pubs.page(params[:page]).per(per_count).to_a
-    @posts.sort{ |x,y| y.created_at <=> x.created_at }
+    @posts  = Post
+              .active_pubs
+              .page(params[:page]).per(params[:per_page])
+
   end
 end
