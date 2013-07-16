7ab4abaa8f937bea03e253539d52c636eb2f639e
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-07-16 20:12:28 +0400

    ~~~

--- config/initializers/the_storages.rb
+++ config/initializers/the_storages.rb
@@ -4,8 +4,8 @@ TheStorages.configure do |config|
   config.watermark_text = 'https://github.com/the-teacher'
   config.convert_path   = '/usr/bin/convert' # BSD: /usr/local/bin/
 
-  config.watermark_font_path  = nil
+  config.watermark_font_path = "#{Rails.root.to_s}/vendor/fonts/georgia_italic.ttf"
 
   config.original_larger_side = 1024
-  config.main_larger_side     = 800
+  config.base_larger_side     = 800
 end
