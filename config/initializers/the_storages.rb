09cb46b5d680a99cd4615965fd00a0e93eea9bff
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-07-16 10:11:15 +0400

    ~~~

---
+++ config/initializers/the_storages.rb
@@ -0,0 +1,6 @@
+# TheStorages.config.param_name => value
+
+TheStorages.configure do |config|
+  config.watermark_text = 'https://github.com/the-teacher'
+  config.convert_path   = 'TEST_VAR'               # BSD: /usr/local/bin/
+end
