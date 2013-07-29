b14cc65fa86750654305042944cb8d6d7bc4f4b5
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-07-29 20:09:31 +0400

    ~~

--- spec/unusfull/users/cabinet.html.haml_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "users/cabinet.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/welcome/index.html.haml_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "welcome/index.html.haml" do
-  # pending "add some examples to (or delete) #{__FILE__}"
-
-  it "root page" do
-    # visit '/'
-  end
-end
