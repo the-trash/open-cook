62f63c78d97717f83de06a8c2197470c37337dfd
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-08-12 21:25:14 +0400

    ~~~

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,6 +35,7 @@ namespace :deploy do
   task :stop  do ; end
   task :restart, roles: :app, except: { no_release: true } do
     p "RESTART SERVER"
+    run "/bin/bash --login"
     run "rvm gemset use open-cook"
     run "rvm gemset name"
   end
