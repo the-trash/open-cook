43784fb14eb50d008be7b49ff14bd4874711ac28
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-08-23 21:44:05 +0400

    ~~~

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,7 @@
+def _join *params
+  params.join ' && '
+end
+
 set :application, "open-cook.ru"
 
 server "zykin-ilya.ru", :app, :web, :db, :primary => true
@@ -16,10 +20,11 @@ set :keep_releases, 10
 default_run_options[:pty]   = true
 default_run_options[:shell] = "/bin/bash --login"
 
-set :gemset_init, 'source "$HOME/.rvm/scripts/rvm" && rvm gemset use open-cook && '
-
 set :ssh_options, { forward_agent: true }
 set :deploy_to,   "#{users_home}/www/#{application}"
+
+set :gemset,    'source "$HOME/.rvm/scripts/rvm" && rvm gemset use open-cook'
+set :rails_env, 'RAILS_ENV=production'
 
 # set :gem,      "#{users_home}/.rvm/rubies/ruby-2.0.0-p247/bin/gem"
 # set :ruby,     "#{users_home}/.rvm/rubies/ruby-2.0.0-p247/bin/ruby"
@@ -34,10 +39,12 @@ namespace :deploy do
   task :start do ; end
   task :stop  do ; end
   task :restart, roles: :app, except: { no_release: true } do
-    p "RESTART SERVER"
-    run gemset_init + "rvm gemset name"
-    run gemset_init + "rvm gemset use open-cook"
-    run gemset_init + "rvm gemset name"
+    run _join gemset_init, rails_env, "rake assets:precompile"
+
+    # p "RESTART SERVER"
+    # run gemset_init + "rvm gemset name"
+    # run gemset_init + "rvm gemset use open-cook"
+    # run gemset_init + "rvm gemset name"
   end
 end
 
