24a9d4a78390d64b3efb2da052b69ae16b62e48a
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-08-24 00:24:02 +0400

    ~~~

--- config/initializers/the_storages.rb
+++ config/initializers/the_storages.rb
@@ -1,12 +1,12 @@
-# TheStorages.config.param_name => value
+TheStorages.config.param_name => value
 
-# TheStorages.configure do |config|
-#   config.watermark_text = 'Open-Cook.ru'
-#   config.convert_path   = '/usr/bin/convert' # BSD: /usr/local/bin/
+TheStorages.configure do |config|
+  config.watermark_text = 'Open-Cook.ru'
+  config.convert_path   = '/usr/bin/convert' # BSD: /usr/local/bin/
 
-#   config.watermark_font_path = "#{Rails.root.to_s}/vendor/fonts/georgia_italic.ttf"
-#   config.watermarks_path     = "#{Rails.root.to_s}/public/uploads/watermarks"
+  config.watermark_font_path = "#{Rails.root.to_s}/vendor/fonts/georgia_italic.ttf"
+  config.watermarks_path     = "#{Rails.root.to_s}/public/uploads/watermarks"
 
-#   config.original_larger_side = 1024
-#   config.base_larger_side     = 800
-# end
+  config.original_larger_side = 1024
+  config.base_larger_side     = 800
+end
