10b34caf4604556bdaf57d58f048895b4e0d25df
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-08-23 23:38:25 +0400

    ~~~

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,9 @@ def _join *params
   params.join ' && '
 end
 
+# =========================================================
+# Params
+# =========================================================
 set :application, "open-cook.ru"
 
 server "zykin-ilya.ru", :app, :web, :db, :primary => true
@@ -27,14 +30,9 @@ set :gemset,    'source "$HOME/.rvm/scripts/rvm" && rvm gemset use open-cook'
 set :rails_env, 'RAILS_ENV=production '
 set :to_app,    "cd " + current_path
 
-
-# release_path # shared_path
-# set :gem,      "#{users_home}/.rvm/rubies/ruby-2.0.0-p247/bin/gem"
-# set :ruby,     "#{users_home}/.rvm/rubies/ruby-2.0.0-p247/bin/ruby"
-# set :rake,     "#{users_home}/.rvm/gems/ruby-2.0.0-p247@global/bin/rake"
-# set :bundle,   "#{users_home}/.rvm/gems/ruby-2.0.0-p247@global/bin/bundle"
-# set :unicorn,  "#{users_home}/.rvm/gems/ruby-2.0.0-p247@global/bin/bundle"
-
+# =========================================================
+# Tasks
+# =========================================================
 namespace :files do
   desc "files:linking"
   task :linking do
@@ -50,6 +48,16 @@ namespace :bundle do
   end
 end
 
+namespace :db do
+  desc "cap deploy db:create"
+  task :create do
+    files.linking
+    run _join [to_app, gemset, rails_env + "rake db:create"]
+  end
+end
+
+# CODE UPDATE
+
 after "files:linking", "deploy:restart", "deploy:cleanup"
 
 namespace :deploy do
@@ -58,12 +66,17 @@ namespace :deploy do
   task :restart, roles: :app, except: { no_release: true } do
     bundle.install
     run _join [to_app, gemset, rails_env + "rake assets:precompile"]
+    
     # p "RESTART SERVER"
     # run gemset_init + "rvm gemset name"
     # run gemset_init + "rvm gemset use open-cook"
     # run gemset_init + "rvm gemset name"
   end
 end
+
+# =========================================================
+# Trash
+# =========================================================
 
 # role :web, application
 # role :app, application
@@ -77,3 +90,10 @@ end
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+# release_path # shared_path
+# set :gem,      "#{users_home}/.rvm/rubies/ruby-2.0.0-p247/bin/gem"
+# set :ruby,     "#{users_home}/.rvm/rubies/ruby-2.0.0-p247/bin/ruby"
+# set :rake,     "#{users_home}/.rvm/gems/ruby-2.0.0-p247@global/bin/rake"
+# set :bundle,   "#{users_home}/.rvm/gems/ruby-2.0.0-p247@global/bin/bundle"
+# set :unicorn,  "#{users_home}/.rvm/gems/ruby-2.0.0-p247@global/bin/bundle"
