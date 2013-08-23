ab63c0185ab9ce02ce3cef342a1744034c66edd0
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-08-24 00:02:04 +0400

    ~~~

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,8 +36,8 @@ set :to_app,    "cd " + current_path
 namespace :files do
   desc "files:linking"
   task :linking do
-    run "ln -nfs #{shared_path}/system              #{current_path}/public/system"
-    run "ln -nfs #{shared_path}/config/database.yml #{current_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/system              #{release_path}/public/system"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
 end
 
@@ -63,7 +63,7 @@ namespace :deploy do
   task :start do ; end
   task :stop  do ; end
   task :restart, roles: :app, except: { no_release: true } do
-    bundle.install
+    # bundle.install
     run _join [to_app, gemset, rails_env + "rake db:migrate"]
     run _join [to_app, gemset, rails_env + "rake assets:precompile"]
     
