cde14e653658b1178261867f5a9b078ad4be1979
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-08-24 00:16:31 +0400

    ~~~

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,8 +35,8 @@ set :to_app,    "cd " + current_path
 # =========================================================
 namespace :deploy do
   task :create_symlink do
-    run "ln -nfs #{shared_path}/system              #{current_path}/public/system"
-    run "ln -nfs #{shared_path}/config/database.yml #{current_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/system              #{release_path}/public/system"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
 end
 
